1689ea23600009cd324ec300491b9c8efcf87320
jonatheorem <jonatheorem@yandex.com>
2024-07-16 14:27:10 -0600

    Added functions to validate command line args, open and close img and write to file.

--- ppm_main.c
+++ ppm_main.c
@@ -2,62 +2,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
+//#include <math.h>
 #include "cimple.h"
 
 int main(int argc, char *argv[]) {
-  FILE *img_file;
   char fname[] = "imagen.ppm";
+  uint32_t width, height, bg_color;
+  ppm img;
 
-  long width, height;
-  uint32_t bg_color = 0x00FFFF; //yellow
-  if (argc < 4) {
-    /* Default image size of 32x32 pixels */
-    width  = 32;
-    height = 32;
-    puts("Image default: 32x32. Color bg yellow.");
-  } else { /* Parse width of image */
-    width  = validate_number(argv[1], MAX_WIDTH, 10);
-    height = validate_number(argv[2], MAX_HEIGHT, 10);
-    bg_color  = validate_number(argv[3], 0xFFFFFFFF, 16);
-    printf("Image size: %ldx%ld.\n", width, height);
-  }
-
-  if ((img_file = fopen(fname, "wb")) == NULL) {
-    fprintf(stderr, "Unable to open %s.", fname);
+  int ok_cmds = validate_cmds(argc, argv, &bg_color, &width, &height);
+  if (ok_cmds < 0) {
+    fprintf(stderr, "Invalid arguments.\n");
     exit(1);
   }
-  
-  char *header = (char *)malloc(sizeof(char)*HEADER_LEN);
-  uint8_t *data_buffer = (uint8_t *)malloc(sizeof(uint8_t)*3*width*height);
-  
-  setup_header(header, width, height);
 
-  ppm img = {.header=header,
-	     .pixels=data_buffer,
-	     .width=width,
-	     .height=height};
+  if ( open_img(&img, width, height) < 0 ) {
+    fprintf(stderr, "Unable to open imagen.\n");
+    exit(2);
+  }
+  
   point or = {.x=1, .y=1};
   
-  fill_background(data_buffer, bg_color, width, height);
+  fill_background(&img, bg_color);
   /*   255, 69, 0, // orange */
   /*   0, 0, 128, // navy blue */
-  point p0 = {(uint32_t)width/2, 1}, p1 = {(uint32_t)width/2, height};
+  point p0 = {width/2, 1}, p1 = {width/2, height};
   line(&img, 0x800000, p0, p1);
   p1.x = width; p1.y = height;
   line(&img, 0x800000, p0, p1);
   p0.x = 1; p0.y = 1;
   line(&img, 0xff00, p0, p1);
 
-  uint32_t red = 0xff;
-  rectangle(&img, &red, or, 5, 5);
+  uint32_t rect_color = 0x00;
+  rectangle(&img, &rect_color, or, 5, 9);
 
-  //g, r, b
-  fwrite(header, sizeof(char), HEADER_LEN, img_file);
-  fwrite(data_buffer, sizeof(uint8_t), 3*width*height, img_file);
-
-  free(header);
-  free(data_buffer);
-  fclose(img_file);
+  write_img_to_file(fname, img);
+  free_ppm(&img);
   return 0;
 }
